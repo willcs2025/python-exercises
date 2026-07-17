3c98f2143022b008f60338ba57627a220a0d4456
willcs2025 <williambernardes882@gmail.com>
2026-07-17 17:02:58 -0300

    Add files via upload

---
+++ 2D list.py
@@ -0,0 +1,8 @@
+
+fruits = ["apple", "orange", "banana", "coconut"] 
+vegetables = ["celery", "carrots", "potatoes"] 
+meats = ["beef", "chicken", "pork"] 
+
+groceries = [fruits, vegetables, meats] 
+
+print(fruits)
